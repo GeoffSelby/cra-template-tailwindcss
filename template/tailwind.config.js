b6f3b4e55eee67116e8cad8651d731dff4d330d9
Geoff Selby <geoff.w.selby@gmail.com>
2020-08-24 19:46:39 -0500

    Remove purgecss dependency and move to Tailwind's built in purge support
    Becaue Tailwind Css has purgecss support built right into core, I've removed the purgecss dependency and moved to utilizing the built in purge support.

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './public/index.html',
+  ],
   theme: {
     extend: {}
   },
